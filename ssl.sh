03828eb8b772cdb875ca0dc56abe0f3dfb7bc004
shuhaibzahir <yesshuhaib.ss@gmail.com>
2021-11-10 14:31:16 +0530

    ssl initial

---
+++ ssl.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo apt install certbot python3-certbot-nginx
+sudo ufw allow 'Nginx Full'
+sudo ufw delete allow 'Nginx HTTP'
+
+echo "Enter the Domain Name: "  
+read domain
+
+sudo certbot --nginx -d  $domain
+
